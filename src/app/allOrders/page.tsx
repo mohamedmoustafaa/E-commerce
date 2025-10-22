5d3401763763401407ab023060fb4a9ae0008456
mohamedmoustafaa <mohamedmooostaf@gmail.com>
2025-10-22 23:26:01 +0300

    fix route

--- src/app/allOrders/page.tsx
+++ src/app/allOrders/page.tsx
@@ -1,9 +1,4 @@
 "use client";
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
-
-
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
